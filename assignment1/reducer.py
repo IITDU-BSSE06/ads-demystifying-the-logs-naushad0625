f456b4c5925c51d8ec719972edec6082527a6b02
Naushad Hossain <bsse0625@iit.du.ac.bd>
2017-11-07 17:48:46 +0600

    Assignment 1 reducer

---
+++ assignment1/reducer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import sys
+
+count = 0
+
+for line in sys.stdin:
+    if line.strip() == "10.99.99.186":
+        count = count + 1
+
+print "Total hit made to 10.99.99.186 : ", int(count)
